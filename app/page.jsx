e96968fbb4e6455e8d296ddca8db570f7a94936e
Rakesh Arora <mannyisha808@gmail.com>
2024-03-29 23:37:31 +0530

    up2

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 'use client'
 import { useEffect, useState, useRef } from 'react'
-import { IoSend } from "react-icons/io5";
+import { IoImages, IoSend } from "react-icons/io5";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import io from 'socket.io-client';
@@ -24,9 +24,18 @@ const Home = () => {
     await fetch("/api/socket");
 
     socket = io();
-    socket.on("newUser",(id)=>{
-      toast("New user joined")
-      console.log("user connected",id);
+    socket.on("newUser", (id) => {
+      toast('New User Joined', {
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+      console.log("user connected", id);
     });
     socket.on("receive-message", (data) => {
       setList(list => [...list, { message: data.message, recv: true }]);
@@ -44,6 +53,9 @@ const Home = () => {
   }
 
   const handleKeypress = (e) => {
+    if (window.screen.width < 500) {
+      return;
+    }
     if (e.key === 'Enter' && !e.shiftKey) {
       handleSend(e);
     }
@@ -57,22 +69,23 @@ const Home = () => {
             {
               list.map((ele, index) => {
                 return <li className={`w-1/3 max-sm:w-1/2 whitespace-pre-line break-words max-sm:mx-[1.5rem] mx-[7rem] p-5 max-sm:p-2 my-2 rounded-lg rounded-tl-xl rounded-br-xl ${ele.recv ? "self-start bg-green-200" : "self-end bg-gray-200"}`}
-                  key={index}>{ele.message}<br /></li>
+                  key={index}>{ele.message}</li>
               })
             }
           </ul>
         </div>
-        <form className='flex justify-center h-[6rem]'>
-          <div className=' w-11/12 max-sm:w-full p-5 flex justify-between'>
-            <textarea className='border scrollbar-none border-black border-opacity-10 resize-none outline-none w-full p-3 rounded-md align-middle shadow-lg shadow-gray-100'
+        <form className='flex justify-center h-[6rem] max-sm:h-[5.7rem]'>
+          <div className=' w-11/12 max-sm:w-full max-sm:px-3 p-5 flex justify-between'>
+            <textarea className='border scrollbar-none border-black border-opacity-10 resize-none outline-none w-full p-3 pl-6 rounded-full align-middle shadow-lg shadow-gray-100'
               type='text'
               value={message}
               onChange={(e) => { setMessage(e.target.value) }} placeholder='Type a message ...'
               onKeyDown={handleKeypress}
             ></textarea>
-            <div className={`px-3 mx-2 cursor-pointer border-none bg-green-500 rounded-lg ${message ? "opacity-100" : "opacity-50"} hover:opacity-50 flex justify-center gap-x-2`} onClick={handleSend}>
-              <button className=' text-white select-none'>Send</button>
-              <IoSend className='self-center text-white select-none' />
+            <div className={`px-3 self-center -ml-20 mx-2 w-[3rem] h-[3rem] max-sm:h-[2.5rem] max-sm:w-[2.5rem] cursor-pointer border-none bg-green-500 rounded-full ${message ? "opacity-100" : "opacity-50"} hover:opacity-50 flex justify-center gap-x-2`} onClick={handleSend}>
+              <button className=' text-white select-none'>
+                <IoSend className='self-center text-white select-none' />
+              </button>
             </div>
           </div>
         </form>
